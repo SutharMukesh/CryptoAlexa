7334932dc89eba6a330f6bcab96fd1aedf1d64f4
Mukesh Suthar <mukeshsuthar336@gmail.com>
2018-09-24 15:19:23 +0530

    code organized made modules for seperate functionality

--- cryptoAlexa.py
+++ cryptoAlexa.py
@@ -1,80 +1,38 @@
-import sqlite3
-import csv
-
+# Library used for ALEXA
 from flask import Flask
 from flask_ask import Ask, statement, question, session
+import processUnit as pu
 
 app = Flask(__name__)
 ask = Ask(app, "/")
-conn = sqlite3.connect('Testdata.db', check_same_thread=False)  # specify database name, if not present it will create on
-c = conn.cursor()
-
-
-def init():  # Create a table, Reads the csv and insert all data to the table.
-    c.execute("DROP TABLE coinsXaction")
-    c.execute("CREATE TABLE coinsXaction(UserName TEXT, Date TEXT, Coins TEXT, BuySell TEXT, Qty REAL, Value REAL, Total REAL)")
-    with open('coinsXaction.csv', 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)  # reads the csv
-        firstLine = True
-        for line in csv_reader:
-            if firstLine:  # skips the first line as it contains header
-                firstLine = False
-                continue
-            c.execute('insert into coinsXaction (UserName, Date, Coins, BuySell, Qty, Value, Total) values (?,?,?,?,?,?,?);', line)
-        conn.commit()
-
-
-def positionQuery(coinName):  # it accepts the coin name, and give the position of a coin
-    data = c.execute("""
-        SELECT sum(Total)
-        FROM coinsXaction
-        WHERE Coins = "{}"
-        """.format(coinName))
-    for line in data:
-        total = int(line[0])
-        if total > 0:
-            return ("Sorry, you are in loss of {} for {} coin".format(total, coinName))
-        else:
-            return (" You are in profit of {} for {} coin".format(total, coinName))
-
-
-def quantity(coinName):  # it gives the available quantity of a coin
-    data = c.execute("""
-        SELECT sum(QTY)
-        FROM coinsXaction
-        WHERE Coins = "{}"
-        """.format(coinName))
-    for line in data:
-        qty = int(line[0])
-        if qty == 0:
-            return ("you sold all your {} coin".format(coinName))
-        else:
-            return (" You've got {} {} coin".format(qty, coinName))
-        break
-
 
 @ask.launch
 def start_skill():  # when alexa opens the app
-    init()
-    welcome = 'Hi mukesh, how can i help?'
+    pu.init()
+    welcome = 'Hi, how can i help?'
     return question(welcome)
 
 
 @ask.intent('AskCoinPrice')
 def getcoin(coinName):
-    message = positionQuery(coinName.upper())
+    message = pu.positionQuery(coinName)
+    return question(message)
+
+
+@ask.intent('AskTopCoin')
+def topCoinsIntent(number):
+    message = pu.topCoins(number)
     return question(message)
 
 
 @ask.intent('availQuantity')
 def getquantity(coinName):
-    message = quantity(coinName.upper())
+    message = pu.quantity(coinName.upper())
     return question(message)
 
 
 if __name__ == '__main__':
     app.run(port=6000, debug=True)  # port should be same on ngrok
 
-
-c.close()  # closing cursors
-conn.close()  # closing db connection
+# pu.c.close()  # closing cursors
+# pu.conn.close()  # closing db connection
--- processUnit.py
+++ processUnit.py
@@ -1,28 +1,23 @@
+import requests  # Library used for getting data from binance api. #coin values
+import json
 import sqlite3
 import csv
-# Library used for getting data from binance api. #coin values
-import json
-from urllib.request import urlopen
-# Library used for ALEXA
-from flask import Flask
-from flask_ask import Ask, statement, question, session
-
-app = Flask(__name__)
-ask = Ask(app, "/")
-conn = sqlite3.connect('Testdata.db', check_same_thread=False)  # specify database name, if not present it will create on
-c = conn.cursor()
 
 url = "https://api.binance.com/api/v1/ticker/allPrices"
+
 # Get all  Price data in json format from binance api
-request = urlopen(url)
-allPrices = json.loads(request.read())  # it stores in json i.e. Dictionary format in python
+allPrices = requests.get(url).json()  # it stores in json i.e. Dictionary format in python
 btcVal = 0  # used to store current bitcoin value
+
+conn = sqlite3.connect('database/Testdata.db', check_same_thread=False)  # specify database name, if not present it will create on
+c = conn.cursor()
 
 
 def init():
     c.execute("DROP TABLE coinsXaction")
     c.execute("CREATE TABLE IF NOT EXISTS coinsXaction(Date TEXT, Market TEXT, Type TEXT, Price REAl, Amount REAL, Total REAL)")
-    with open('TradeHistory.csv', 'r') as csv_file:
+
+    with open('database/TradeHistory.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         firstLine = True
         for line in csv_reader:
@@ -47,6 +42,7 @@ def init():
                 btcVal = dictionary['price']
 
         conn.commit()
+
 
 def getCurrPrice(coin):
     coin = coin.upper() + "BTC"
@@ -98,7 +94,7 @@ def positionQuery(coinName):
     # available quantity of a coin
 
         if usdProfLossPrice > 0:
-            return (" You are in profit of {:.1f} dollars ".format(usdProfLossPrice))    
+            return (" You are in profit of {:.1f} dollars ".format(usdProfLossPrice))
         else:
             return ("Sorry, you are in loss of {:.1f}".format(usdProfLossPrice))
 
@@ -117,36 +113,3 @@ def quantity(coinName):  # it gives the available quantity of a coin
         else:
             return (" You've got {} {} coin".format(qty, coinName))
         break
-
-
-@ask.launch
-def start_skill():  # when alexa opens the app
-    init()
-    welcome = 'Hi mukesh, how can i help?'
-    return question(welcome)
-
-
-@ask.intent('AskCoinPrice')
-def getcoin(coinName):
-    message = positionQuery(coinName)
-    return question(message)
-
-
-@ask.intent('AskTopCoin')
-def topCoinsIntent(number):
-    message = topCoins(number)
-    return question(message)
-
-
-@ask.intent('availQuantity')
-def getquantity(coinName):
-    message = quantity(coinName.upper())
-    return question(message)
-
-
-if __name__ == '__main__':
-    app.run(port=6000, debug=True)  # port should be same on ngrok
-
-
-c.close()  # closing cursors
-conn.close()  # closing db connection
